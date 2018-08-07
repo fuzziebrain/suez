60242f7d21494002d6765907e0c06ab89882cf3d
Martin D'Souza <martin@clarifit.com>
2018-08-06 23:57:28 -0600

    cleanup for docker mode

--- app.js
+++ app.js
@@ -6,10 +6,21 @@ var app = express();
 
 var config = require('./config/settings.json');
 
+process.argv.forEach(function (val, index, array) {
+  console.log(index + ': ' + val);
+});
 
-config.docker = config.docker == undefined ? false : config.docker;
+// *** Docker specific settings ***
+// If run like "node app.js docker" then it will trigger the app to be run in docker mode
+config.docker = process.argv[2] == undefined ? false : true;
+console.log(`config.docker: ${config.docker}`);
+// If in docker mode then need to allow listen from all IPs (not just localhost)
 config.listenHost = config.docker ? '' : '127.0.0.1';
-console.log(`${config.listenHost}`);
+console.log(`config.listenHost: ${config.listenHost}`);
+
+// Docker relies on port 3000 (exposed port)
+config.service.port = config.docker ? 3000 : config.service.port;
+console.log(`config.service.port: ${config.service.port}`);
 
 app.use('/', proxy(
   {
@@ -17,7 +28,6 @@ app.use('/', proxy(
     changeOrigin: true,
     router: function(req) {
       var routeName = req.hostname.split('.')[0];
-      console.log(`routeName: ${routeName}`);
 
       var proxyTarget = config.apiTargets.find(
         function(apiTarget) {
@@ -26,12 +36,9 @@ app.use('/', proxy(
         routeName
       ).proxyTarget;
 
-      console.log(`${proxyTarget}`);
-
       return proxyTarget;
     }
   }
 ));
 
-// app.listen(config.service.port, '127.0.0.1');
 app.listen(config.service.port, config.listenHost);
